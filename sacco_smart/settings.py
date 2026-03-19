19214f97b57f318406a17f01a82d3c4cf4a841a1
23landus <23landus@gmail.com>
2026-03-19 18:04:21 +0300

    Connected to PostgreSQL database

--- sacco_smart/settings.py
+++ sacco_smart/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +80,11 @@ WSGI_APPLICATION = 'sacco_smart.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # This keeps your local work on SQLite, but uses Postgres on Render
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
